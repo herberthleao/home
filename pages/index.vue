024a6ee213040a6c68155ff6e019bb51a51e80bb
Herberth Leão <herberth.leao@pm.me>
2021-12-15 09:39:06 -0300

    Add index page title

--- pages/index.vue
+++ pages/index.vue
@@ -3,3 +3,15 @@
     <views-the-splash />
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  head() {
+    return {
+      title: "Herberth Leão"
+    }
+  }
+});
+</script>
