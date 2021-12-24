1cc3e23692c569faff54b1e2cad167f1b8e6a946
Herberth Leão <herberth.leao@pm.me>
2021-12-24 12:27:34 -0300

    Fix duplicate script tag

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,80 +7,78 @@
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue";
-
-export default defineComponent({
-  setup() {
-    const splash = useSplash();
-
-    return { splash };
-  }
-});
-</script>
-
 <style lang="scss">
 .page-container-main {
   background: $color-gray-dark;
 }
 </style>
 
-<script lang="ts" setup>
-useMeta({
-  meta: [
-    {
-      name: "author",
-      content: "Herberth Leão, herberth@leao.sh"
-    },
-    {
-      name: "msapplication-TileColor",
-      content: "#DA532C"
-    },
-    {
-      name: "msapplication-config",
-      content: "/assets/images/icons/favicons/browserconfig.xml"
-    },
-    {
-      name: "theme-color",
-      content: "#FFFFFF"
-    },
-    {
-      link: "apple-touch-icon",
-      sizes: "180x180",
-      href: "/assets/images/icons/favicons/apple-touch-icon.png"
-    }
-  ],
-  link: [
-    {
-      rel: "apple-touch-icon",
-      sizes: "180x180",
-      href: "/assets/images/icons/favicons/apple-touch-icon.png"
-    },
-    {
-      rel: "icon",
-      type: "image/png",
-      sizes: "32x32",
-      href: "/assets/images/icons/favicons/favicon-32x32.png"
-    },
-    {
-      rel: "icon",
-      type: "image/png",
-      sizes: "16x16",
-      href: "/assets/images/icons/favicons/favicon-16x16.png"
-    },
-    {
-      rel: "manifest",
-      href: "/assets/images/icons/favicons/site.webmanifest"
-    },
-    {
-      rel: "mask-icon",
-      href: "/assets/images/icons/favicons/safari-pinned-tab.svg",
-      color: "#5BBAD5"
-    },
-    {
-      rel: "shortcut icon",
-      href: "/assets/images/icons/favicons/favicon.ico"
-    }
-  ]
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  setup() {
+    useMeta({
+      meta: [
+        {
+          name: "author",
+          content: "Herberth Leão, herberth.leao@pm.me"
+        },
+        {
+          name: "msapplication-TileColor",
+          content: "#DA532C"
+        },
+        {
+          name: "msapplication-config",
+          content: "/assets/images/icons/favicons/browserconfig.xml"
+        },
+        {
+          name: "theme-color",
+          content: "#FFFFFF"
+        },
+        {
+          link: "apple-touch-icon",
+          sizes: "180x180",
+          href: "/assets/images/icons/favicons/apple-touch-icon.png"
+        }
+      ],
+      link: [
+        {
+          rel: "apple-touch-icon",
+          sizes: "180x180",
+          href: "/assets/images/icons/favicons/apple-touch-icon.png"
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "32x32",
+          href: "/assets/images/icons/favicons/favicon-32x32.png"
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "16x16",
+          href: "/assets/images/icons/favicons/favicon-16x16.png"
+        },
+        {
+          rel: "manifest",
+          href: "/assets/images/icons/favicons/site.webmanifest"
+        },
+        {
+          rel: "mask-icon",
+          href: "/assets/images/icons/favicons/safari-pinned-tab.svg",
+          color: "#5BBAD5"
+        },
+        {
+          rel: "shortcut icon",
+          href: "/assets/images/icons/favicons/favicon.ico"
+        }
+      ]
+    });
+
+    const splash = useSplash();
+
+    return { splash };
+  }
 });
 </script>
