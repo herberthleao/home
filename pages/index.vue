61c2c0d88a6d01af9b9a23a06289a60a13beb1be
Herberth Leão <herberth.leao@pm.me>
2021-12-28 20:36:36 -0300

    Add Maven Pro font

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,20 @@
 <template>
-  <div></div>
+  <div class="container">
+    <main class="">
+      <div class="main-section-greeting">Olá, meu nome é</div>
+      <div>Herberth Leão.</div>
+      <div>Eu resolvo problemas usando <span>PHP</span></div>
+      <p></p>
+    </main>
+  </div>
 </template>
+
+<style lang="scss">
+.main-section-greeting {
+  color: $color-gray-medium;
+  font-family: $second-font;
+}
+</style>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
