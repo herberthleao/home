191c66ba6d2378f78e2e8a200fd0aa2045afc896
Herberth Leão <herberth.leao@pm.me>
2021-12-28 21:22:18 -0300

    Add main section

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="container">
-    <main class="">
+    <main class="lg:py-16 lg:px-20 py-16 px-8">
       <div class="main-section-greeting">Olá, meu nome é</div>
-      <div>Herberth Leão.</div>
-      <div>Eu resolvo problemas usando <span>PHP</span></div>
+      <div class="main-section-name">Herberth Leão.</div>
+      <div class="main-section-sub">Eu resolvo problemas usando <span>PHP</span></div>
       <p></p>
     </main>
   </div>
@@ -11,8 +11,28 @@
 
 <style lang="scss">
 .main-section-greeting {
-  color: $color-gray-medium;
+  @apply leading-10;
+
+  color: $second-color;
   font-family: $second-font;
+}
+
+.main-section-name {
+  @apply font-bold;
+  @apply leading-10;
+  @apply text-5xl;
+
+  color: $color-white;
+  font-family: $main-font;
+}
+
+.main-section-sub {
+  @apply font-bold;
+  @apply leading-10;
+  @apply text-5xl;
+
+  color: $color-gray-light;
+  font-family: $main-font;
 }
 </style>
 
